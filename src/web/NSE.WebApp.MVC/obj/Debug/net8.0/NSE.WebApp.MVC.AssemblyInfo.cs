53d65d0e8b8fa88ea87a4cda9bf43b514bf126fd
Vinícius Henriques <viniciuspinholi6@gmail.com>
2025-02-22 16:11:04 -0300

    Configs acesso SQL Server Express Local via Container Docker

--- src/web/NSE.WebApp.MVC/obj/Debug/net8.0/NSE.WebApp.MVC.AssemblyInfo.cs
+++ src/web/NSE.WebApp.MVC/obj/Debug/net8.0/NSE.WebApp.MVC.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NSE.WebApp.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7011ca1c0892511790a6e3851485e4becaa1ac6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87603e7f13a7a6695321685843bb69eb2c163e49")]
 [assembly: System.Reflection.AssemblyProductAttribute("NSE.WebApp.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NSE.WebApp.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
