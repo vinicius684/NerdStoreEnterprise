0a0b522177b446498740f532169df2ac783ea52a
Vinícius Henriques <viniciuspinholi6@gmail.com>
2025-03-04 23:19:58 -0300

    Implementação NGINX ptII

--- src/web/NSE.WebApp.MVC/obj/Debug/net8.0/NSE.WebApp.MVC.AssemblyInfo.cs
+++ src/web/NSE.WebApp.MVC/obj/Debug/net8.0/NSE.WebApp.MVC.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NSE.WebApp.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9761387dac370856107e16c4c18ba8ac7ed13c8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2cadedf4200154d118932ecdc3b227c4a75b050")]
 [assembly: System.Reflection.AssemblyProductAttribute("NSE.WebApp.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NSE.WebApp.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
