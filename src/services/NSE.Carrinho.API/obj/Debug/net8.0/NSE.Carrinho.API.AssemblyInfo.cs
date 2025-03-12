3c8c6d80f0f07142863a146a40f13bdc8446bf63
Vinícius Henriques <viniciuspinholi6@gmail.com>
2025-03-12 17:18:27 -0300

    Implementado Load Balancing no NGINX

--- src/services/NSE.Carrinho.API/obj/Debug/net8.0/NSE.Carrinho.API.AssemblyInfo.cs
+++ src/services/NSE.Carrinho.API/obj/Debug/net8.0/NSE.Carrinho.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NSE.Carrinho.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de5284b0abce5a956b1b4989ad5896de381cdd65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fca26ff6d91e48cdd2fab3b31748c97887af5a49")]
 [assembly: System.Reflection.AssemblyProductAttribute("NSE.Carrinho.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NSE.Carrinho.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
